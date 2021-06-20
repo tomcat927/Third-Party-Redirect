2e7734f17a081ec99a0992655f34e006ec2ef1f5
Petit-Abba <964103426@qq.com>
2021-06-20 17:28:03 +0800

    0

--- service.sh
+++ service.sh
@@ -1,13 +1,12 @@
 #!/system/bin/sh
 MODDIR="$(dirname $(readlink -f "$0"))"
-ServiceSh="1"
 
 until [[ $(getprop sys.boot_completed) -eq 1 ]]; do
 	sleep 2
 done
 
 sdcard_rw() {
-	local test_file="/sdcard/Android/.B_TEST_FILE"
+	local test_file="/sdcard/Android/.Redirect_test"
 	touch $test_file
 	while [[ ! -f $test_file ]]; do
 		touch $test_file
@@ -25,12 +24,48 @@ ROOTS() {
 	chown root:root $1
 }
 
+Network_Connection() {
+	if [[ $(ping -c 1 1.2.4.8) ]] >/dev/null 2>&1; then
+		echo 0
+	elif [[ $(ping -c 1 8.8.8.8) ]] >/dev/null 2>&1; then
+		echo 0
+	elif [[ $(ping -c 1 114.114.114.114) ]] >/dev/null 2>&1; then
+		echo 0
+	else
+		echo 1
+	fi
+}
+
 sdcard_rw
 
 if [[ "$(cat $MODDIR/files/Variable.sh | grep "$PATH")" == "" ]]; then
 	echo "" >> $MODDIR/files/Variable.sh
-	echo "# $(date "+%Y-%m-%d %H:%M:%S")" >> $MODDIR/files/Variable.sh
 	echo "PATH=\"$PATH:/system/sbin:/sbin/.magisk/busybox:$(magisk --path)/.magisk/busybox\"" >> $MODDIR/files/Variable.sh
+fi
+
+
+[[ -f $MODDIR/Cloud_Redirect.sh ]] && rm -rf $MODDIR/Cloud_Redirect.sh
+
+. $MODDIR/files/Variable.sh
+
+until [[ $(Network_Connection) == 0 ]]; do
+	sleep 5
+done
+
+cd $MODDIR
+
+if [[ ! -z $(which curl) ]]; then
+	curlwget="curl"
+	until [[ -f $MODDIR/Redirect.prop ]]; do
+		curl -O 'https://gitee.com/Petit-Abba/Third-Party-Redirect/raw/master/Cloud_Redirect.sh' >/dev/null 2>&1
+		sleep 2
+	done
+elif [[ ! -z $(which wget) ]]; then
+	curlwget="wget"
+	until [[ -f $MODDIR/Redirect.prop ]]; do
+		wget 'https://gitee.com/Petit-Abba/Third-Party-Redirect/raw/master/Cloud_Redirect.sh' >/dev/null 2>&1
+		sleep 2
+	done
 fi
 
 ROOTS $MODDIR/files/Author_Information/QQGroup
